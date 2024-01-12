91544e4df9b23ac23f1746e2bc3e4dda15a8de59
akshatcoder-hash <akshatcoder@outlook.com>
2024-01-13 02:37:08 +0530

    users struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+
+type Result<T> = std::result::Result<T, error::Error>;
+type WebResult<T> = std::result::Result<T, Rejection>;
+type Users = Arc<RwLock<HashMap<String, User>>>;
+
+#[derive(Clone)]
+pub struct User {
+    pub uid: String,
+    pub email: String,
+    pub pw: String,
+    pub role: String,
 }
+
