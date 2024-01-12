9163c68348a4ae5906e4a32bbb1e273fbc04ebc9
akshatcoder-hash <akshatcoder@outlook.com>
2024-01-13 02:59:23 +0530

    create users

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
+use serde::de::Unexpected::Str;
 
 type Result<T> = std::result::Result<T, error::Error>;
 type WebResult<T> = std::result::Result<T, Rejection>;
@@ -13,3 +14,30 @@ pub struct User {
     pub role: String,
 }
 
+#[tokio::main]
+async fn main() {
+    let users = Arc::new(RwLock::new(init_users()));
+}
+
+fn init_users() -> HashMap<String, User> {
+    let mut map = HashMap::new();
+    map.insert(
+        String::from("1"),
+        User{
+            uid: String::from("1"),
+            email: String::from("akshatsharma0023@outlook.com"),
+            pw: String::from("1234"),
+            role:String::from("Admin"),
+        },
+    );
+    map.insert(
+        String::from("2"),
+        User{
+            uid: String::from("2"),
+            email: String::from("akshatsharma.sol@outlook.com"),
+            pw: String::from("1234"),
+            role: String::from("User"),
+        },
+    );
+    map
+}
